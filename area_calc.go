d3022d8ab52af278e3af51f26280f334f0d90c55
LuminareHQ <noah@noahw.design>
2024-02-06 12:03:36 -0500

    Update area_calc.go

--- area_calc.go
+++ area_calc.go
@@ -4,28 +4,54 @@ import (
 	"fmt"
 	"math"
 	"strconv"
+	"time"
 )
 
-var multiplier float64 = 4.0
+var ratio = 4.0
+
+var noError = false
 
 func main() {
-	println("Type 'exit' or 'CTRL + C' To Quit.")
+	println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	println("Type 'exit' Or 'CTRL + C' To Quit.")
+
+	for noError == false {
+		print("Enter Ratio Value: ")
+		var _, err = fmt.Scan(&ratio)
+
+		if err != nil {
+			println("Ratio Must Be A Number.")
+			ratio = 4.0
+		} else {
+			noError = true
+		}
+	}
+
+	println(fmt.Sprintf("Ratio Set: %.0f:1", ratio))
 	println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 	var input string
 	for {
 		print("Enter Material Thickness: ")
 		var _, err = fmt.Scan(&input)
 		if err != nil {
-			panic(err)
+			println("Application Panicked.")
+			input = "0"
+		}
+
+		if input == "exit" {
+			println("Exiting...")
+			time.Sleep(1 * time.Second)
+			break
 		}
 
 		var thickness, err2 = strconv.ParseFloat(input, 16)
 
 		if err2 != nil {
-			panic(err2)
+			println("Thickness Must Be A Number.")
+			thickness = 0
 		}
 
-		area := multiplier * math.Pi * math.Pow(thickness, 2)
+		area := ratio * math.Pi * math.Pow(thickness, 2)
 
 		println(fmt.Sprintf("Calculated Output Area: %.4f", area))
 		println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
